47745b89cbcac3e05b0fec834817722bf1d9a4e9
dario.schaffner <darioschaffner7@gmail.com>
2023-02-02 11:06:23 +0100

    wip

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  @Output()
+ public buttonClicked: EventEmitter<any> = new EventEmitter();
+
+}
